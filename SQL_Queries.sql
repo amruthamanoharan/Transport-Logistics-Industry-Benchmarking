ab1a78addf406ecb84484020b74ada7c6d6a5db1
Amrutha Manoharan <122939423+amruthamanoharan@users.noreply.github.com>
2025-01-30 22:03:37 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -1,1 +1,29 @@
+** SQL queries used in the project **
+
+1️⃣ Identify the Most Profitable Companies
+SELECT TOP 5 Companies.CompanyName, FinancialMetrics.OperatingProfitMargin
+FROM Companies
+INNER JOIN FinancialMetrics ON Companies.CompanyID = FinancialMetrics.CompanyID
+ORDER BY FinancialMetrics.OperatingProfitMargin DESC;
+
+
+2️⃣ Revenue Trends by Industry
+SELECT Companies.Industry, AVG(FinancialMetrics.Revenue) AS AvgRevenue
+FROM Companies
+INNER JOIN FinancialMetrics ON Companies.CompanyID = FinancialMetrics.CompanyID
+GROUP BY Companies.Industry
+ORDER BY AvgRevenue DESC;
+
+
+3️⃣ Fleet Size vs. On-Time Performance
+SELECT Companies.CompanyName, PerformanceKPIs.FleetSize, PerformanceKPIs.OnTimePerformance
+FROM Companies
+INNER JOIN PerformanceKPIs ON Companies.CompanyID = PerformanceKPIs.CompanyID
+ORDER BY PerformanceKPIs.OnTimePerformance DESC;
+
+4️⃣ Most Operationally Efficient Companies
+SELECT TOP 5 Companies.CompanyName, PerformanceKPIs.AverageLoadFactor, PerformanceKPIs.OnTimePerformance
+FROM Companies
+INNER JOIN PerformanceKPIs ON Companies.CompanyID = PerformanceKPIs.CompanyID
+ORDER BY PerformanceKPIs.AverageLoadFactor DESC, PerformanceKPIs.OnTimePerformance DESC;
 
